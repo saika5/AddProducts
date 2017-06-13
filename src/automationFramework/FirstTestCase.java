78f83f2ef9bac31bb24a48386e2bfd4ba2fbbe4f
saika5 <saika.ahmed64@gmail.com>
2017-06-12 19:07:56 -0500

    one more changes

--- src/automationFramework/FirstTestCase.java
+++ src/automationFramework/FirstTestCase.java
@@ -5,7 +5,7 @@ public class FirstTestCase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello to git");
-
+        System.out.println("Reflecting changes");
 	}
 
 }
